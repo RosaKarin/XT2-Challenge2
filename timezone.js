951cfd539198c03d6f4318a1063e9a6e71767822
RosaKarin <50404764+RosaKarin@users.noreply.github.com>
2022-03-15 16:32:11 +0100

    n

--- timezone.js
+++ timezone.js
@@ -3,16 +3,19 @@
 function updateTimeFrame(){
 	var now = new Date();
 	var h = now.getHours();
+    var background = document.getElementsByClassName('scenery');
 		
 	if (h >= 6 && h < 8){ //Sunrise
-        document.getElementById('body').style.backgroundColor = ('#ffa47f') //sunrise
+        document.getElementBy('body').style.backgroundColor = ('#ffa47f') //sunrise
         document.getElementById('zon').style.background = 'radial-gradient(circle at top left, #ffd753, #e21b1b)'; //sunrise
         document.getElementById('keyframe').style.boxShadow = '0px 0px 5px #ffd753'; //sunrise
+        
 	
     } else if (h >= 8 && h < 17){ 	//Day
         document.getElementById('body').style.background = 'linear-gradient(#60dbf9, #ffa47f)'; //day
         document.getElementById('zon').style.background = 'radial-gradient(circle at top left, #e21b1b. #ffd753)'; // day
         document.getElementById('keyframe').style.boxShadow = '0px 0px 5px white'; //day
+        
     
     } else if (h >= 15 && h <18) { //sundown
         document.getElementById('body').style.backgroundColor = ('#e76243') //sundown
